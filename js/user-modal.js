0a75d9a8509aceb07068f0f29b9a7aa3b9c5cf11
Gaukhar Kashaganova <gaukharka@personal-mac-gaukhar.home>
2021-02-18 21:13:45 +0100

    still not updating

--- js/user-modal.js
+++ js/user-modal.js
@@ -1,7 +1,6 @@
 import {isEscEvent, isEnterEvent} from './util.js';
 import { pictures } from './data.js';
 
-
 const pictureCards = document.querySelectorAll('.picture__img');
 const picturePreviewModal = document.querySelector('.big-picture');
 const closeButton = document.querySelectorAll('.big-picture__cancel');
@@ -23,40 +22,33 @@ const userCloseModal = () => {
   document.removeEventListener('keydown', onPopupEscKeydown);
 }
 
+// const bigPicturePreview = document.querySelector('.big-picture__preview');   //mojno udalit pozje
+
 //open modal
 pictureCards.forEach((card) => {
   card.addEventListener('click', (evt) => {
     evt.preventDefault();
     userOpenModal();
 
-    const bigPicturePreview = document.querySelector('.big-picture__preview');
     const bigPictureImg = document.querySelector('.big-picture__img');
-
-    const photoId = card.dataset.id;
-    const myPhotoData = pictures.find(photo => photo.id === photoId);
-
-    bigPicturePreview.dataset.id = myPhotoData;
     const bigPicture = bigPictureImg.querySelector('img');
     bigPicture.src = card.src;
+    const photoId = card.dataset.id;
+    const bigPictureData = pictures.find(photo => photo.id = photoId);
+    picturePreviewModal.querySelector('.likes-count').textContent = bigPictureData.likes;
+    picturePreviewModal.querySelector('.comments-count').textContent = bigPictureData.comments.length;
+    picturePreviewModal.querySelector('.social__caption').textContent = bigPictureData.description;
+    picturePreviewModal.querySelector('.social__picture').src = 'bigPictureData.comments.avatar';
+    picturePreviewModal.querySelector('.social__picture').alt = 'bigPictureData.comments.name';
+    picturePreviewModal.querySelector('.social__text').textContent = bigPictureData;
 
-    pictures.forEach((photo) => {
-      bigPicturePreview.querySelector('.likes-count').textContent = photo.likes;
-      bigPicturePreview.querySelector('.comments-count').textContent = photo.comments.length;
-      bigPicturePreview.querySelector('.social__caption').textContent = photo.description;
-      bigPicturePreview.querySelector('.social__picture').src = photo.comments.avatar;
-      bigPicturePreview.querySelector('.social__picture').alt = photo.comments.name;
-      bigPicturePreview.querySelector('.social__text').textContent = photo.comments.message;
-    });
-
-    // delete     UNCOMMENT LATER
-    // const commentCounts = bigPicturePreview.querySelector('.social__comment-count');
+    // UNCOMMENT LATER
+    // const commentCounts = picturePreviewModal.querySelector('.social__comment-count');
     // commentCounts.classList.add('hidden');
-    // const commentLoader = bigPicturePreview.querySelector('.comments-loader');
+    // const commentLoader = picturePreviewModal.querySelector('.comments-loader');
     // commentLoader.classList.add('hidden');
-    document.body.classList.add('modal-open');
+    // document.body.classList.add('modal-open');
   });
-
-
 
   card.addEventListener('keydown', (evt) => {
     if (isEnterEvent(evt)) {
@@ -71,3 +63,4 @@ closeButton.forEach((card) => {
     userCloseModal();
   })
 });
+
