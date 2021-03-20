6924f7bcc0cd1d5124961cf3f1eeedbb2fe7f6fd
Gaukhar Kashaganova <gaukharka@personal-mac-gaukhar.home>
2021-03-20 15:06:14 +0100

    added alert message on server fail

--- source/js/get-data.js
+++ source/js/get-data.js
@@ -3,14 +3,27 @@ import {openBigPictureModal} from './big-picture.js';
 import {renderSmallPictures} from './small-pictures.js';
 import {submitForm} from './send-data.js';
 
+const ALERT_TIME = 5000;
+
 const filtersContainer = document.querySelector('.img-filters');
 
+const displayAlert = () => {
+  const messageContainer = document.createElement('div');
+  messageContainer.classList.add('error-display');
+  messageContainer.textContent = 'Ошибка сервера!';
+  document.body.append(messageContainer);
+  setTimeout(() => {
+    messageContainer.remove();
+  }, ALERT_TIME);
+}
+
 const getData = (onSuccess) => {
-  fetch('https://22.javascript.pages.academy/kekstagram/data')
+  fetch('https://22.javascript.pages.academy/kekstagram/')
     .then((response) => response.json())
     .then((data) => {
       onSuccess(data)
-    });
+    })
+    .catch(() => displayAlert());
 };
 
 getData((data) => {
